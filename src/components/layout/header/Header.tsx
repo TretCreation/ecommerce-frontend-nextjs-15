02266d5c0871472479758df804567374f9ec3778
TretCreation <tret.creation@gmail.com>
2025-01-25 20:19:12 +0200

    add: style header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,8 +1,13 @@
-import React from "react";
-// import styles from "./Header.module.scss";
+import React from "react"
+import styles from "./Header.module.scss"
 
 const Header = () => {
-  return <div className={""}>Header</div>;
-};
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.menu}>Tret.Shop</div>
+      <div className={styles.menuItem}></div>
+    </div>
+  )
+}
 
-export default Header;
+export default Header
