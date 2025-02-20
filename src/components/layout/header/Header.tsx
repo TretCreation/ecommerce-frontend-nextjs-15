64f44dff93ecae7ba290eb8720089ed6857027a6
TretCreation <tret.creation@gmail.com>
2025-02-20 17:29:41 +0200

    add: navbar

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,12 +1,24 @@
 import React from "react"
 import styles from "./Header.module.scss"
+import Link from "next/link"
+import DownArrow from "@/public/icons/down-arrow.svg"
 
 const Header = () => {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.menu}>Tret.Shop with css</div>
-      <div className={styles.menuItem}>default Tret.Shop</div>
-    </div>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <p></p>
+      </div>
+      <nav className={styles.nav}>
+        <Link href={"/"}>
+          Shop
+          <DownArrow />
+        </Link>
+        <Link href={"/"}>On Sale</Link>
+        <Link href={"/"}>New Arrivals</Link>
+        <Link href={"/"}>Brands</Link>
+      </nav>
+    </header>
   )
 }
 
