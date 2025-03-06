27aac1f46b37046b1d2fcaa4c05ec759e05059d6
TretCreation <tret.creation@gmail.com>
2025-03-06 19:17:27 +0200

    add: ui Button

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,8 +1,19 @@
+import cn from "classnames"
 import React from "react"
 import styles from "./Button.module.scss"
+import { IButtonProps } from "./Button.props"
 
-const Button = () => {
-  return <button className={styles.btn}></button>
-}
-
+const Button = ({ appearance, size, children, className, ...props }: IButtonProps): React.JSX.Element => (
+  <button
+    className={cn(className, styles.button, {
+      [styles.primary]: appearance === "primary",
+      [styles.secondary]: appearance === "secondary",
+      [styles.medium]: size === "medium",
+      [styles.large]: size === "large"
+    })}
+    {...props}
+  >
+    {children}
+  </button>
+)
 export default Button
