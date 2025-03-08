261876853d4e8d354f9462ade51902994af8d3ff
TretCreation <tret.creation@gmail.com>
2025-03-08 22:10:23 +0200

    add: brands (HeroSection)

--- src/components/home/hero-section/HeroSection.tsx
+++ src/components/home/hero-section/HeroSection.tsx
@@ -1,6 +1,12 @@
 import React from "react"
 import styles from "./HeroSection.module.scss"
 import Image from "next/image"
+import Button from "@/components/ui/button/Button"
+import VersaceIcon from "@/public/icons/versace.svg"
+import ZaraIcon from "@/public/icons/zara.svg"
+import GucciIcon from "@/public/icons/gucci.svg"
+import PradaIcon from "@/public/icons/prada.svg"
+import CalvinKleinIcon from "@/public/icons/calvin-klein.svg"
 
 const HeroSection = () => {
   return (
@@ -10,7 +16,30 @@ const HeroSection = () => {
         Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater
         to your sense of style.
       </p>
-      <div className={styles.brands}></div>
+      <Button appearance='primary' size='medium'>
+        Shop Now
+      </Button>
+      <div className={styles["hero__stats"]}>
+        <div className={styles.stat}>
+          <div className={styles.number}>200+</div>
+          <div className={styles.label}>International Brands</div>
+        </div>
+        <div className={styles.stat}>
+          <div className={styles.number}>2,000+</div>
+          <div className={styles.label}>High-Quality Products</div>
+        </div>
+        <div className={styles.stat}>
+          <div className={styles.number}>30,000+</div>
+          <div className={styles.label}>Happy Customers</div>
+        </div>
+      </div>
+      <div className={styles.brands}>
+        <VersaceIcon />
+        <ZaraIcon />
+        <GucciIcon />
+        <PradaIcon />
+        <CalvinKleinIcon />
+      </div>
       <Image alt='' src='/images/hero.png' width={1440} height={663} />
     </section>
   )
